1d0839855bb063f78f1becaf9f1b647f1d76cec4
SandeepBara <sandeepbara830@gmail.com>
2025-01-13 15:07:07 +0530

    update transporter

--- app/Models/TransporterDetail.php
+++ app/Models/TransporterDetail.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class TransporterDetail extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "transporter_name",
+        'lock_status',
+    ];
+
+    public function store($request){        
+        $inputs = snakeCase($request);
+        $id= self::create($inputs->all())->id;
+        return $id;
+    }
+
+    public function edit($request){
+        $inputs = snakeCase($request)->filter(function($val,$index){
+            return (in_array($index,$this->fillable));
+        });
+        $return= self::where("id",$request->id)->update($inputs->all());
+        return $return;
+    }
+
+    public function getAutoListOrm(){
+        return self::where("lock_status",false);
+    }
 }
